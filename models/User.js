38fa1588e1a65c3aa9f6c083221731fc7ef846ee
mava1 <anamariamaldonado1994@gmail.com>
2024-05-25 13:15:56 -0400

    week14

--- models/User.js
+++ models/User.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
+//const jwt = require("jsonwebtoken"); ------>>>We aren’t using JWTs in this project.
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -31,7 +31,9 @@ UserSchema.pre("save", async function () {
   this.password = await bcrypt.hash(this.password, salt);
 });
 
-UserSchema.methods.createJWT = function () {
+/*----->>>>We aren’t using JWTs in this project.<<<<----- */
+
+/*UserSchema.methods.createJWT = function () {
   //return jwt.sign({ userId: this._id, name: this.name }, "jwtSecret", {expiresIn: "30d", });
   return jwt.sign(
     { userId: this._id, name: this.name },
@@ -40,7 +42,7 @@ UserSchema.methods.createJWT = function () {
       expiresIn: process.env.JWT_LIFETIME,
     }
   );
-};
+};*/
 
 UserSchema.methods.comparePassword = async function (canditatePassword) {
   const isMatch = await bcrypt.compare(canditatePassword, this.password);
